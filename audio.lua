55b75db66752f97b38f31ea1caa55614099ef673
Jens Genberg <jens@greenbyte.com>
2019-09-16 20:34:11 +0200

    Document audio.lua

--- audio.lua
+++ audio.lua
@@ -1,48 +1,52 @@
+-- Responsible for creating audio sources and handling playback of music and sfx
 local audio = {}
 local sfx = {}
-local songs = {}
+local songPhases = {}
 
+-- Creates and sets up audio sources from files - run once at startup
 function audio.init()
     sfx.die = love.audio.newSource("content/die.wav", "static")
     sfx.eatApple = love.audio.newSource("content/eatApple.wav", "static")
     sfx.bang = love.audio.newSource("content/bang.wav", "static")
     sfx.reset = love.audio.newSource("content/reset.wav", "static")
 
-    songs.activeSong = love.audio.newSource("content/song_active.ogg", "static")
-    songs.activeSong:setLooping(true)
-    songs.activeSong:setVolume(0)
+    -- Set all song phases to loop with zero volume
+    songPhases.activesongPhase = love.audio.newSource("content/songPhase_active.ogg", "static")
+    songPhases.activesongPhase:setLooping(true)
+    songPhases.activesongPhase:setVolume(0)
 
-    songs.passiveSong = love.audio.newSource("content/song_passive.ogg",
-                                             "static")
-    songs.passiveSong:setLooping(true)
-    songs.passiveSong:setVolume(0)
+    songPhases.passivesongPhase = love.audio.newSource("content/songPhase_passive.ogg", "static")
+    songPhases.passivesongPhase:setLooping(true)
+    songPhases.passivesongPhase:setVolume(0)
 
-    songs.gameoverSong = love.audio.newSource("content/song_gameover.ogg",
-                                              "static")
-    songs.gameoverSong:setLooping(true)
-    songs.gameoverSong:setVolume(0)
+    songPhases.gameoversongPhase = love.audio.newSource("content/songPhase_gameover.ogg", "static")
+    songPhases.gameoversongPhase:setLooping(true)
+    songPhases.gameoversongPhase:setVolume(0)
 end
 
+-- Restarts all song phases and switches to the passive version of the song phase
 function audio.start()
-    for _, v in pairs(songs) do
+    for _, v in pairs(songPhases) do
         love.audio.stop(v)
         love.audio.play(v)
     end
 
-    audio.toggleSong("passiveSong")
+    audio.togglesongPhase("passivesongPhase")
 end
 
+-- Plays a sound effect, after first making sure it's stopped
 function audio.play(source)
     love.audio.stop(sfx[source])
     love.audio.play(sfx[source])
 end
 
-function audio.toggleSong(source)
-    for k, v in pairs(songs) do v:setVolume(0) end
-    print(source)
-    songs[source]:setVolume(1)
+-- Makes sure only the requested source is unmuted
+function audio.togglesongPhase(source)
+    for _, v in pairs(songPhases) do v:setVolume(0) end
+    songPhases[source]:setVolume(1)
 end
 
-function audio.getSongTime() return songs.activeSong:tell() end
+-- Returns the current time of the song loop (could use any song phase here, since they start at the same time)
+function audio.getsongPhaseTime() return songPhases.activesongPhase:tell() end
 
 return audio
